13aae3799f16b60756efbae744de0211e1d44e4e
Gilbert <steven.n.gilbert@gmail.com>
2017-06-12 09:38:15 -0400

    chore: Set up starter files

---
+++ main.js
@@ -0,0 +1,32 @@
+//const data = 'https://raw.githubusercontent.com/FreeCodeCamp/ProjectReferenceData/master/GDP-data.json';
+const data = 'gdp.json';
+const request = new Request(data);
+let gdp = [];
+
+
+/*fetch(request)
+  .then(function(response) { return response.json(); })
+  .then(function(data) {
+    gpp = data.data;
+  });
+  */
+
+
+fetch(request, {
+  mode: 'no-cors'
+}).then(function(response) { return response.json(); })
+  .then(function(data) {
+    gpp = data.data;
+    console.log(gdp);
+  });
+
+//var data = [4, 8, 15, 16, 23, 42];
+
+/*d3.select("#bar-chart")
+  .selectAll("div")
+    .data(data)
+  .enter().append("div")
+    .style("width", function(d) { return d * 10 + "px"; })
+    .text(function(d) { return d; });*/
+
+
